726d8331a3e1f3c4a09e3d01e9b6c943d6afcd86
Ali Kianoor <askianoor@gmail.com>
2020-02-21 15:00:08 +0330

    Initial Commit - Add Bootstrap and fundamental Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './_Components/homepage/homepage.component';
+import { AboutMeComponent } from './_Components/about-me/about-me.component';
+import { PortfolioComponent } from './_Components/portfolio/portfolio.component';
+import { NavbarComponent } from './_Components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AboutMeComponent,
+    PortfolioComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
