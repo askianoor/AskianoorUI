d8215216d794b51c71843e0bb6a8f68ca298f333
Ali Kianoor <askianoor@gmail.com>
2020-02-21 20:08:17 +0330

    Add ParticleEffect and Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './_Components/homepage/homepage.component';
+import { AboutMeComponent } from './_Components/about-me/about-me.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Home',
+    component: HomepageComponent,
+  },
+  {
+    path: 'About',
+    component: AboutMeComponent,
+  },
+  // {
+  //   path: 'login',
+  //   component: LoginComponent,
+  // },
+  // {
+  //   path: 'register',
+  //   component: RegisterFormComponent,
+  // },
+  { path: '', redirectTo: '/Home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/Home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
