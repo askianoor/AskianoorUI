c25ef93e7a6654d75b792f79d54f6ee40529db91
Ali Kianoor <askianoor@gmail.com>
2020-02-28 16:07:30 +0330

    Add Navbar items and skills

--- src/app/_Components/navbar/navbar.component.ts
+++ src/app/_Components/navbar/navbar.component.ts
@@ -10,7 +10,11 @@ export class NavbarComponent implements OnInit {
   menu = [
     { id: 0, name: 'Home', path: '/home' },
     { id: 1, name: 'About Me', path: '/About'  },
-    { id: 2, name: 'Portfolio', path: '/Portfolio'  }
+    { id: 2, name: 'Portfolio', path: '/Portfolio'  },
+    { id: 2, name: 'Skills', path: '/Skills'  },
+    { id: 2, name: 'Experience', path: '/Experience'  },
+    { id: 2, name: 'Education', path: '/Education'  },
+    { id: 2, name: 'Contact Me', path: '/Contact'  },
   ];
 
   constructor() { }
